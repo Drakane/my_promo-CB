5d8c62579b9a9cb8c4f1b3ba1ef715ccb8bce422
byron <byron.cambien@hotmail.com>
2025-04-07 13:51:31 +0200

    update

---
+++ meme.js
@@ -0,0 +1,6 @@
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("click", function () {
+    console.log("H1 cliqué !");
+    window.location.href = "https://www.youtube.com/watch?v=ZZ5LpwO-An4&ab_channel=ProtoOfSnagem";
+});
