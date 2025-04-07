a820fc84d6d5d2b8d55c55f799e7947042c80f84
byron <byron.cambien@hotmail.com>
2025-04-07 14:05:37 +0200

    update

--- meme.js
+++ meme.js
@@ -2,5 +2,5 @@ const h1 = document.querySelector("h1");
 
 h1.addEventListener("click", function () {
     console.log("H1 cliqué !");
-    window.location.href = "https://www.youtube.com/watch?v=ZZ5LpwO-An4&ab_channel=ProtoOfSnagem";
+    window.location.href = "https://www.youtube.com/watch?v=HC7CiSQR2vE&ab_channel=Enrikeco";
 });
